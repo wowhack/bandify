88cc22ac2e2088293ec3d13c37a36665d4374b00
Joakim Persson <joakimperssson@gmail.com>
2014-08-05 18:21:26 +0200

    More audio recording

--- public/track.js
+++ public/track.js
@@ -1,262 +1,70 @@
-function supportsMedia() {
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
-        navigator.mozGetUserMedia || navigator.msGetUserMedia);
-}
+(function(window) {
+  var client = new BinaryClient('ws://localhost:9001');
 
-// cross-browser support for getUserMedia
-navigator.getUserMedia = navigator.getUserMedia ||
-    navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia ||
-    navigator.msGetUserMedia;
+  client.on('open', function() {
+    window.Stream = client.createStream();
 
-window.URL = window.URL || window.webkitURL;
+    if (!navigator.getUserMedia)
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-window.requestAnimationFrame = (function () {
-    return  window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame
-})();
+    if (navigator.getUserMedia) {
+      navigator.getUserMedia({audio:true}, success, function(e) {
+        alert('Error capturing audio.');
+      });
+    } else alert('getUserMedia not supported in this browser.');
 
-window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    var recording = false;
 
-
-var mediaStream;
-
-// global variables for showing/encoding the video
-var mediaInitialized = false;
-var recording = false;
-var videoCanvas;
-var videoContext;
-var frameTime;
-var imageArray = [];
-
-// global variables for recording audio
-var audioContext;
-var audioRecorder;
-
-// function for requesting the media stream
-function setupMedia() {
-    if (true) {
-        audioContext = new AudioContext();
-
-        navigator.getUserMedia(
-            {
-                video: true,
-                audio: true
-            },
-            function (localMediaStream) {
-                // map the camera
-                var video = document.getElementById('live_video');
-                video.src = window.URL.createObjectURL(localMediaStream);
-
-                // create the canvas & get a 2d context
-                videoCanvas = document.createElement('canvas');
-                videoContext = videoCanvas.getContext('2d');
-
-                // setup audio recorder
-                var audioInput = audioContext.createMediaStreamSource(localMediaStream);
-                //audioInput.connect(audioContext.destination);
-                // had to replace the above with the following to mute playback
-                // (so you don't get feedback)
-                var audioGain = audioContext.createGain();
-                audioGain.gain.value = 0;
-                audioInput.connect(audioGain);
-                audioGain.connect(audioContext.destination);
-
-                audioRecorder = new Recorder(audioInput);
-                mediaStream = localMediaStream;
-                mediaInitialized = true;
-
-                document.getElementById('uploading').hidden = true;
-                
-                document.getElementById('record').hidden = false;
-            },
-            function (e) {
-                console.log('web-cam & microphone not initialized: ', e);
-                document.getElementById('media-error').hidden = false;
-            }
-        );
+    window.startRecording = function() {
+      recording = true;
     }
-};
 
-
-var jp_start = function() {
-
-  console.log("click #start-recording");
-
-  document.getElementById('stop-recording').disabled = false;
-  document.getElementById('start-recording').disabled = true;
-  startRecording();
-  
-}
-
-var jp_stop = function() {
-    
-  console.log("click #stop-recording");
-
-  document.getElementById('stop-recording').disabled = true;
-  document.getElementById('start-recording').disabled = false;
-  stopRecording();
-    
-}
-
-function startRecording() {
-    console.log("Begin Recording");
-
-    videoElement = document.getElementById('live_video');
-    videoCanvas.width = videoElement.width;
-    videoCanvas.height = videoElement.height;
-
-    imageArray = [];
-
-    // do request frames until the user stops recording
-    recording = true;
-    frameTime = new Date().getTime();
-    requestAnimationFrame(recordFrame);
-
-    // begin recording audio
-    audioRecorder.record();
-}
-
-function stopRecording() {
-    console.log("End Recording");
-    recording = false;
-}
-
-function completeRecording() {
-    // stop & export the recorder audio
-    audioRecorder.stop();
-
-   
-    console.log("completeRecording: ");
-
-    document.getElementById('uploading').hidden = false;
-
-    audioRecorder.exportWAV(function (audioBlob) {
-        // save to the db
-        BinaryFileReader.read(audioBlob, function (err, fileInfo) {
-            //TODO send post?
-            /*
-            UserAudios.insert({
-                userId: user._id,
-                audio: fileInfo,
-                save_date: Date.now()
-            });
-            */
-            $.ajax({
-              type: 'POST',
-              url: '/tracks/test',
-              data: {data: fileInfo},
-              success: function(data, status) {
-                console.log(data);
-                console.log(status);
-              }
-            });
-            console.log(fileInfo);
-        });
-        console.log("Audio uploaded");
-        
-    });
-
-    // do the video encoding
-    // note: tried doing this in real-time as the frames were requested but
-    // the result didn't handle durations correctly.
-    var whammyEncoder = new Whammy.Video();
-    for (i in imageArray) {
-        videoContext.putImageData(imageArray[i].image, 0, 0);
-        whammyEncoder.add(videoContext, imageArray[i].duration);
-        delete imageArray[i];
+    window.stopRecording = function() {
+      recording = false
+      window.Stream.end();
     }
-    var videoBlob = whammyEncoder.compile();
 
-    BinaryFileReader.read(videoBlob, function (err, fileInfo) {
-        //TODO send post?
-        console.log(fileInfo)
-        /*
-        UserVideos.insert({
-            userId: user._id,
-            video: fileInfo,
-            save_date: Date.now()
-        });
-        
-        $.ajax({
-          type: 'POST',
-          url: '/tracks/test',
-          data: {data: fileInfo},
-          success: function(data, status) {
-            console.log(data);
-            console.log(status);
-          }
-        })
-*/
-        //console.log('video');
-        //console.log(fileInfo);
-    });
-    console.log("Video uploaded");
+    function success(e) {
+      audioContext = window.AudioContext || window.webkitAudioContext;
+      context = new audioContext();
 
-    // stop the stream & redirect to show the video
-    mediaStream.stop();
-    console.log('Now we are done');
-}
+      // the sample rate is in context.sampleRate
+      audioInput = context.createMediaStreamSource(e);
 
-function recordFrame() {
-//    console.log("-frame");
+      var bufferSize = 2048;
+      recorder = context.createScriptProcessor(bufferSize, 1, 1);
 
-    if (recording) {
-        var image;
-        // draw the video to the context, then get the image data
-        var video = document.getElementById('live_video');
-        var width = video.width;
-        var height = video.height;
-        videoContext.drawImage(video, 0, 0, width, height);
+      recorder.onaudioprocess = function(e){
+        if(!recording) return;
+        console.log ('recording');
+        var left = e.inputBuffer.getChannelData(0);
+        window.Stream.write(convertoFloat32ToInt16(left));
+      }
 
-        // optionally get the image, do some filtering on it, then
-        // put it back to the context
-        imageData = videoContext.getImageData(0, 0, width, height);
-        // - do some filtering on imageData
-        videoContext.putImageData(imageData, 0, 0);
-
-        var frameDuration = new Date().getTime() - frameTime;
-
-        console.log("duration: " + frameDuration);
-        //whammyEncoder.add(videoContext, frameDuration);
-        imageArray.push(
-            {
-                duration: frameDuration,
-                image: imageData
-            });
-        frameTime = new Date().getTime();
-
-        // request another frame
-        requestAnimationFrame(recordFrame);
+      audioInput.connect(recorder)
+      recorder.connect(context.destination); 
     }
-    else {
-        completeRecording();
+
+    function convertoFloat32ToInt16(buffer) {
+      var l = buffer.length;
+      var buf = new Int16Array(l)
+
+      while (l--) {
+        buf[l] = buffer[l]*0xFFFF;    //convert to 16 bit
+      }
+      return buf.buffer
     }
-}
+  });
+})(this);
 
-
-var BinaryFileReader = {
-    read: function (file, callback) {
-        var reader = new FileReader;
-
-        var fileInfo = {
-            name: file.name,
-            type: file.type,
-            size: file.size,
-            file: null
-        }
-
-        reader.onload = function () {
-            fileInfo.file = new Uint8Array(reader.result);
-            callback(null, fileInfo);
-        }
-        reader.onerror = function () {
-            callback(reader.error);
-        }
-
-        reader.readAsArrayBuffer(file);
-    }
-}
+(function($) {
+  $('#track_start').on('click', function(e) {
+    e.preventDefault();
+    console.log(e);
+  });
+  $('#track_stop').on('click', function(e) {
+    e.preventDefault();
+    console.log(e);
+  })
+})(jQuery);
--- server.js
+++ server.js
@@ -3,24 +3,13 @@ var express = require('express');
 var mongoose = require('mongoose');
 var getRawBody = require('raw-body')
 var typer      = require('media-typer')
+var BinaryServer = require('binaryjs').BinaryServer;
+var fs = require('fs');
+var wav = require('wav');
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-
-app.use(function (req, res, next) {
-  getRawBody(req, {
-    length: req.headers['content-length'],
-    limit: '10000mb',
-    encoding: 'utf-8'
-  }, function (err, string) {
-    if (err)
-      return next(err)
-
-    req.text = string
-    next()
-  })
-});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -55,5 +44,25 @@ app.post('/tracks/save', track.save);
 app.get('/tracks/:id', track.show)
 app.get('/example', example.index);
 
-app.post('/tracks/test', track.test);
 app.listen(3000);
+
+
+// Audio streaming
+binaryServer = BinaryServer({port: 9001});
+
+binaryServer.on('connection', function(client) {
+  var fileWriter = new wav.FileWriter('sound/demo' + Math.floor((Math.random() * 100000)) + '.wav', {
+    channels: 1,
+    sampleRate: 48000,
+    bitDepth: 16
+  });
+
+  client.on('stream', function(stream, meta) {
+    
+    stream.pipe(fileWriter);
+    
+    stream.on('end', function() {
+      fileWriter.end();
+    });
+  });
+});
