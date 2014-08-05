76297b6770e5eac6925de33d54fbcab339152df1
Joakim Persson <joakimperssson@gmail.com>
2014-08-05 11:39:57 +0200

    Added example structure

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 
 var app = express();
+var example = require('./controllers/example');
+
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
@@ -10,5 +13,9 @@ app.set('view engine', 'jade');
 app.get('/', function(req, res) {
 	res.render('hello');
 });
+
+app.get('/example', example.index);
+
+
 
 app.listen(3000);
