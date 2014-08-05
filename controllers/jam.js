17fae00b018636ce0b5d45d9841b9f57d7ae211c
Niklas Andréasson <andreasson.n@gmail.com>
2014-08-05 14:27:51 +0200

    Fix stuff that Krobbe DIDNT

--- controllers/jam.js
+++ controllers/jam.js
@@ -5,6 +5,7 @@ exports.index = function(req, res) {
   Jam.find({}, function(err, jams) {
     res.render('jam/list', { jams: jams });  
   });
+};
 
 // return a form for creating a new jam
 exports.create = function(req, res) {
@@ -16,10 +17,10 @@ exports.create = function(req, res) {
 // user fills in jam name, presses create gets sent here
 
 // we create a jam with the title, then redirect to jam specific route
-
 exports.save = function(req, res) {
   var newJam = new Jam({ title: req.body.title });
 
   newJam.save(function(err, jam) {
     res.redirect('/jam');
   });
+};
