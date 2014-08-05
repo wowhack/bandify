54b443f4fbd053cbd9d3c92d71ebe2e110f031a0
Robin Andersson <k.robin.andersson@gmail.com>
2014-08-05 22:33:14 +0200

    Remove crap ass unnecessary lines

--- controllers/user.js
+++ controllers/user.js
@@ -12,20 +12,6 @@ exports.signup = function(req, res) {
     res.render('signup', { message: req.flash('signupMessage') });
 };
 
-// exports.save = function(req, res) {
-
-//   var newUser = new User({name: req.body.name});
-
-//   newUser.save(function(err, product) {
-
-//     if (err) {
-//       console.error(err);
-//     }
-
-//     res.redirect('/user');
-//   });
-// }
-
 exports.show = function(req, res) {
   res.render('user/index', {
     user : req.user // get the user out of session and pass to template
