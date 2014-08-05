70fcffcaed22151908da39bf240c43264dad277a
adderollen <andreas.rolen@ctk.se>
2014-08-05 14:36:01 +0200

    Fix jam model error

--- models/jam.js
+++ models/jam.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var JamSchema = new Schema({
-  title:  String
-  owner:  {type: Schema.ObjectId, ref: 'User'}
-  spotifyId:  String
-  password:  String
+  title:  String,
+  owner:  {type: Schema.ObjectId, ref: 'User'},
+  spotifyId:  String,
+  password:  String,
   tracks:  [{type: Schema.ObjectId, ref 'Track'}]
 });
 
