7dd756891c8785f5ff943707276df507a9aa476e
adderollen <andreas.rolen@ctk.se>
2014-08-06 15:10:22 +0200

    fixed list jam

--- public/list-jam.js
+++ public/list-jam.js
@@ -2,26 +2,39 @@
 
 function printList(data) {
 	data.jams.forEach(function(jam) {
-		$(".jam-list").append('<li class="jam-list-item"><p>'+jam.title+'<a href="/jam/'+jam._id+'" class="btn btn-default">View Jam</a><a href="/jam/delete/'+jam._id+'" class="btn btn-danger">Delete</a></li>')
+		$(".jam-list").append('<li class="jam-list-item" id="' + jam._id +'" ><p data-name="jam-list-item">'+jam.title+'</p></li>')
 	});
 	
 }
 
+$(document).on('click', 'p', function(evt) {
+	if($(evt.target).data('name') == 'jam-list-item') {
+		window.location.assign("/jam/" + evt.target.parentNode.id);
+	}
+})
+
 
 $(document).on('click', 'button', function(evt) {
-	
+	console.log('evt', $(evt.target).data('name'));
 	if($(evt.target).data('name') == 'jam-search') {
 		if(document.getElementById('jam-search-field').value == "") {
 			$.get('/jam/getall', function(data) {
+				$(".jam-results").show();
 				$(".jam-list").empty();
 				printList(data);
 			});
 		}
 		else {
 			$.get('/jam/search/jams/' + document.getElementById('jam-search-field').value, function(data) {
-				$(".jam-list").empty();
-				printList(data);
+				$(".jam-results").show();
+				if(data.jams.length > 0) {
+					$(".jam-list").empty();
+					printList(data);
+				}
 			});
 		}
+	}
+	if($(evt.target).data('name') == 'spotify') {
+		window.location.assign("/jam/search");
 	}
 });
